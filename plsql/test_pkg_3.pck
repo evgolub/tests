5168a728035f4eccd0df962d7461e7463f2364fa
Golub Evgenii <evgenii.golub.94@gmail.com>
2018-03-21 03:51:49 +0800

    tests commit 5

---
+++ plsql/test_pkg_3.pck
@@ -0,0 +1,24 @@
+CREATE OR REPLACE PACKAGE test_pkg_3 IS
+
+TYPE CURSOR_REC2 IS RECORD (id int, name varchar2(200), city_id int);
+
+TYPE CURSOR2 IS TABLE OF CURSOR_REC2;
+
+FUNCTION GET_NAME RETURN CURSOR2 PIPELINED;
+
+END test_pkg_3;
+/
+CREATE OR REPLACE PACKAGE BODY test_pkg_3 IS
+FUNCTION GET_NAME RETURN CURSOR2 PIPELINED IS
+
+BEGIN
+
+FOR cur2 IN (select id, name, city_id from people)
+    LOOP
+        PIPE ROW(cur2);
+    END LOOP;
+RETURN;
+END GET_NAME;
+
+END test_pkg_3;
+/
--- src/com/haron/ConnectionUtils.java
+++ src/com/haron/ConnectionUtils.java
@@ -5,46 +5,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-////import com.haron.*;
-
 public class ConnectionUtils {
 
     public static Connection getMyConnection() throws SQLException,
             ClassNotFoundException {
 
-        // Using Oracle
-        // You may be replaced by other Database.
+
         return OracleConnUtils.getOracleConnection();
     }
-
-
 
         public static void main(String[] args)  throws ClassNotFoundException,
                 SQLException {
 
-            //+ Get Connection
             Connection connection = ConnectionUtils.getMyConnection();
 
-            // Create statement
             Statement statement = connection.createStatement();
 
-            String sql = "SELECT id, name, city_id FROM people";
+            String sql = "SELECT * from table(test_pkg_3.GET_NAME)";
 
-            // Execute SQL statement returns a ResultSet object.
             ResultSet rs = statement.executeQuery(sql);
 
-            // Fetch on the ResultSet
-            // Move the cursor to the next record.
             while (rs.next()) {
-                int id = rs.getInt(1);
+                String id = rs.getString(1);
                 String name = rs.getString(2);
-                int city_id = rs.getInt(3);
-                System.out.println("--------------------");
-
-                System.out.println("people_id:" + id);
-                System.out.println("name:" + name);
-                System.out.println("city_id:" + city_id );
-
+                String City_id = rs.getString(3);
+                System.out.println("----------------------------------------");
+                System.out.println("id:" + id + " name:" + name + " city_id:" + City_id);
             }
             rs.close();
             // Close connection.
