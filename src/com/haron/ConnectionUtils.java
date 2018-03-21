21587372a9357c5636acdde85d8bbab902e7a925
Golub Evgenii <evgenii.golub.94@gmail.com>
2018-03-22 01:46:39 +0800

    spec,body,xml ver.1

--- src/com/haron/ConnectionUtils.java
+++ src/com/haron/ConnectionUtils.java
@@ -1,9 +1,19 @@
 package com.haron;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class ConnectionUtils {
 
@@ -18,25 +28,60 @@ public class ConnectionUtils {
                 SQLException {
 
             Connection connection = ConnectionUtils.getMyConnection();
-
             Statement statement = connection.createStatement();
-
             String sql = "SELECT * from table(test_pkg_3.GET_NAME)";
-
             ResultSet rs = statement.executeQuery(sql);
 
-            while (rs.next()) {
-                String name = rs.getString(1);
-                String c_name = rs.getString(2);
-                String f_name = rs.getString(3);
-                String v_name = rs.getString(4);
-                System.out.println("-------------------------");
-                System.out.println("Имя: " + name + "\nГород: " + c_name + "\nФрукт: " + f_name + "\nОвощ: " + v_name);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder;
+            try {
+                builder = factory.newDocumentBuilder();
+
+                Document doc = builder.newDocument();
+
+                Element rootElement = doc.createElement("root");
+
+                doc.appendChild(rootElement);
+
+                while (rs.next()) {
+                    rootElement.appendChild(getLanguage(doc, rs.getString(1), rs.getString(2),rs.getString(3), rs.getString(4)));
+                }
+                connection.close();
+
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+
+                StreamResult file = new StreamResult(new File("D:/people.xml"));
+
+                transformer.transform(source, file);
+
+                System.out.println("Создание XML файла закончено");
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            rs.close();
-            // Close connection.
-            connection.close();
         }
 
+    private static Node getLanguage(Document doc, String name, String city, String fruit, String vegatable) {
+        Element people = doc.createElement("People");
+
+        people.appendChild(getPeopleElement(doc, people, "Имя", name));
+        people.appendChild(getPeopleElement(doc, people, "Город", city));
+        people.appendChild(getPeopleElement(doc, people, "Фрукт", fruit));
+        people.appendChild(getPeopleElement(doc, people, "Овощ", vegatable));
+
+        return people;
     }
+
+    private static Node getPeopleElement(Document doc, Element element, String name, String value) {
+        Element node = doc.createElement(name);
+        node.appendChild(doc.createTextNode(value));
+        return node;
+    }
+}
+
+
+
+
 
